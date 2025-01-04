b3dc377ed6266c7a99959295124cf45fa192030b
Hirohisa Honmo <65598223+HirohisaHonmo@users.noreply.github.com>
2025-01-04 13:32:29 +0000

    Refactor: Minor modification.

--- src/calculate_dumbbell_weight/calculate_dumbbell_weight.py
+++ src/calculate_dumbbell_weight/calculate_dumbbell_weight.py
@@ -2,16 +2,16 @@ from itertools import combinations
 
 from tabulate import tabulate
 
-PLATE_WEIGHTS = [1.25, 2.0, 2.5, 3.5]
-TWO_PLATES_FOR_PAIR = 2
+_PLATE_WEIGHTS = [1.25, 2.0, 2.5, 3.5]
+_TWO_PLATES_FOR_PAIR = 2
+_HEADERS = ["dumbbell_weight", "plates"]
 all_combinations = []
 
-for i in range(1, len(PLATE_WEIGHTS) + 1):
-    for subset in combinations(PLATE_WEIGHTS, i):
-        total_weight = sum(subset) * TWO_PLATES_FOR_PAIR
+for i in range(1, len(_PLATE_WEIGHTS) + 1):
+    for subset in combinations(_PLATE_WEIGHTS, i):
+        total_weight = sum(subset) * _TWO_PLATES_FOR_PAIR
         all_combinations.append([total_weight, ", ".join(map(str, subset))])
 
 all_combinations.sort(key=lambda x: x[0])
 
-headers = ["dumbbell", "plates"]
-print(tabulate(all_combinations, headers=headers, tablefmt="fancy_grid"))
+print(tabulate(all_combinations, headers=_HEADERS, tablefmt="fancy_grid"))
