72278e4616e2cddf8fe841119b6dff917dfa1bb7
Hirohisa Honmo <65598223+HirohisaHonmo@users.noreply.github.com>
2025-08-03 10:56:02 +0000

    Refactor: Simplify logger.

--- src/consolidate_csv_to_excel/custom_logger.py
+++ src/consolidate_csv_to_excel/custom_logger.py
@@ -1,54 +1,41 @@
 import logging
 import sys
-from logging import Logger
+from logging import Formatter, Logger, StreamHandler
 from logging.handlers import TimedRotatingFileHandler
 from pathlib import Path
 
-_LOG_FILE_PATH = Path(__file__).parent / "log" / "test.log"
+_DEFAULT_LOG_FILE_PATH = Path(__file__).parent / "log" / "default.log"
 
 
 class CustomLogger:  # pragma: no cover
-    _instance: Logger | None = None
-
     @classmethod
     def get_logger(
         cls,
-        log_file_path: Path = _LOG_FILE_PATH,
+        name: str,
+        log_file_path: Path = _DEFAULT_LOG_FILE_PATH,
         log_level: int = logging.INFO,
         when: str = "midnight",
         backup_count: int = 7,
     ) -> Logger:
-        if cls._instance is None:
-            cls._instance = cls._initialize_logger(
-                log_file_path, log_level, when, backup_count
-            )
-        return cls._instance
-
-    @classmethod
-    def _initialize_logger(
-        cls,
-        log_file_path: Path,
-        log_level: int,
-        when: str,
-        backup_count: int,
-    ) -> Logger:
-        logger = logging.getLogger(__name__)
+        logger = logging.getLogger(name)
         logger.setLevel(log_level)
 
-        file_handler = TimedRotatingFileHandler(
-            log_file_path, when=when, backupCount=backup_count
-        )
-        file_handler.setLevel(log_level)
-        formatter = logging.Formatter(
-            fmt="%(asctime)s - %(levelname)s - %(message)s",
-            datefmt="%Y-%m-%d %H:%M:%S",
-        )
-        file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
+        if not logger.hasHandlers():
+            formatter = Formatter(
+                fmt="%(asctime)s - %(levelname)s - %(module)s.%(funcName)s - %(message)s",  # noqa E501
+                datefmt="%Y-%m-%d %H:%M:%S",
+            )
 
-        console_handler = logging.StreamHandler(sys.stdout)
-        console_handler.setLevel(log_level)
-        console_handler.setFormatter(formatter)
-        logger.addHandler(console_handler)
+            file_handler = TimedRotatingFileHandler(
+                log_file_path, when=when, backupCount=backup_count
+            )
+            file_handler.setFormatter(formatter)
+            file_handler.setLevel(log_level)
+            logger.addHandler(file_handler)
+
+            stream_handler = StreamHandler(sys.stdout)
+            stream_handler.setFormatter(formatter)
+            stream_handler.setLevel(log_level)
+            logger.addHandler(stream_handler)
 
         return logger
