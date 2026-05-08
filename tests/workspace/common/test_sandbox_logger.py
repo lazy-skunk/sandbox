714055389f55491088983346ec6827d1ac87cf4b
Hirohisa Honmo <65598223+HirohisaHonmo@users.noreply.github.com>
2026-05-08 16:08:46 +0000

    test: fix

--- tests/workspace/common/test_sandbox_logger.py
+++ tests/workspace/common/test_sandbox_logger.py
@@ -1,36 +1,22 @@
 import logging
 from pathlib import Path
-from unittest.mock import Mock
 
 from pytest import MonkeyPatch
 
-from workspace.common.sandbox_logger import SandboxLogger
+from workspace.common import sandbox_logger
 
 
 def test_get_logger(monkeypatch: MonkeyPatch, tmp_path: Path) -> None:
     # Arrange
-    experiment_name = "test_dir"
-    logger = logging.getLogger(experiment_name)
+    logger = logging.getLogger(sandbox_logger.LOGGER_NAME)
     logger.handlers.clear()
     logger.propagate = False
 
-    monkeypatch.setattr(SandboxLogger, "_DEFAULT_SANDBOX_PATH", tmp_path)
-    monkeypatch.setattr(
-        "workspace.common.sandbox_logger.currentframe", lambda: None
-    )
-    monkeypatch.setattr(
-        "workspace.common.sandbox_logger.getouterframes",
-        lambda _frame: [
-            None,
-            None,
-            Mock(filename=str(tmp_path / experiment_name / "test.py")),
-        ],
-    )
-
-    log_path = tmp_path / experiment_name / "log" / "test_dir.log"
+    monkeypatch.setattr(sandbox_logger, "LOG_DIR", tmp_path)
+    log_path = tmp_path / f"{sandbox_logger.LOGGER_NAME}.log"
 
     # Act
-    logger = SandboxLogger.get_logger()
+    logger = sandbox_logger.get_logger()
     logger.info("test")
 
     # Assert
